14c921b7c86e2f5132a3d7c26199526d5b8d9811
Phillip Stier <pstier@installatron.com>
2024-02-11 16:16:11 -0500

    Update auth.inc.php
    #1

--- incl/auth.inc.php
+++ incl/auth.inc.php
@@ -9,7 +9,7 @@ ini_set('session.use_trans_sid', 0);
 if (isset($session_save_path)) session_save_path($session_save_path);
 session_start();
 
-if (isset($_SESSION['session_username']) && $_SESSION['session_username'] == $username && isset($_SESSION['session_password']) && ( $_SESSION['session_password'] === md5($password) || $_SESSION['session_password'] === $password ) || !$phpfm_auth);
+if ( isset($_SESSION['session_username']) && $_SESSION['session_username'] === $username && isset($_SESSION['session_password']) && ( strlen($_SESSION['session_password']) === 32 && $_SESSION['session_password'] === md5($password) || password_verify($password, $_SESSION['session_password']) || $_SESSION['session_password'] === $password ) || !$phpfm_auth);
 else exit("<font color='#CC0000'>Access Denied!</font>");
 
 ?>
